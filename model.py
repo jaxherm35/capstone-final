247b089d6fa0bd8d7f756e8b27e5e3de0d71b4cf
jaxherm35 <jaxherm35@gmail.com>
2022-07-12 15:13:45 -0600

    fourth commit

--- model.py
+++ model.py
@@ -2,14 +2,41 @@ import email
 from sqlite3 import connect
 from unicodedata import name
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask
+from sqlalchemy import PrimaryKeyConstraint
+from server import app
 
 db = SQLAlchemy()
+
+# from app import db, create_app
+# app = create_app()
+# app.app_context().push()
+
+
+def create_app():
+    app = Flask(__name__)
+    db.init_app(app)
+    return app
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(80), unique=True)
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 
 class Sits(db.Model):
 
     __tablename__ = "sits"
 
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     address = db.Column(db.String(80))
     phone_number = db.Column(db.String(80), nullable=True)
@@ -22,10 +49,25 @@ class Sits(db.Model):
     panels = db.Column(db.Integer())
     notes = db.Column(db.String(300))
 
+    def __init__(self, name, address, phone_number, email, avg_bill, total_kwh, new_price_with, new_price_without, offset, panels, notes):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
+        self.avg_bill = avg_bill
+        self.total_kwh = total_kwh
+        self.new_price_with = new_price_with
+        self.new_price_without = new_price_without
+        self.offset = offset
+        self.panels = panels
+        self.notes = notes
+
+
 class Sold(db.Model):
 
     __tablename__ = "sold"
 
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     address = db.Column(db.String(80))
     phone_number = db.Column(db.String(80), nullable=True)
@@ -41,6 +83,22 @@ class Sold(db.Model):
     date_sold = db.Column(db.String(20))
     notes = db.Column(db.String(300))
 
+    def __init__(self, name, address, phone_number, email, avg_bill, total_kwh, new_price_with, new_price_without, offset, panels, loan_provider, interest_rate, date_sold, notes):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
+        self.avg_bill = avg_bill
+        self.total_kwh = total_kwh
+        self.new_price_with = new_price_with
+        self.new_price_without = new_price_without
+        self.offset = offset
+        self.panels = panels
+        self.loan_provider = loan_provider
+        self.interest_rate = interest_rate
+        self.date_sold = date_sold
+        self.notes = notes
+
 
 def connect_to_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Jaxson:sqlpassword@localhost:5432/capstone-final'
@@ -50,7 +108,7 @@ def connect_to_db(app):
 
 
 if __name__ == "__main__":
-    from server import app
+    db.create_all()
     connect_to_db(app)
     print("Connected to database")
 
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from xmlrpc.client import _HostType
+# from xmlrpc.client import _HostType
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db
@@ -11,4 +11,5 @@ app.secret_key = "very_secret_key"
 if __name__ == '__main__':
     app.env = 'development'
     connect_to_db(app)
-    app.run(port=5000, host='localhost', debug=True)    
+    DebugToolbarExtension(app)
+    app.run(port=5000, host='0.0.0.0', debug=True)    
